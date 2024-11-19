1c4ed7c9a3ef50a3c5c57b579dd60ff89abdabaf
rael5 <rael1up@gmail.com>
2024-11-19 07:27:54 -0400

    Styling Navbar

---
+++ components/navbar.jsx
@@ -0,0 +1,31 @@
+"use client"
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+
+function NavButton({ href, children}) {
+    return (
+      <motion.div className="mx-4 hover:underline inline-block"
+      initial = {{ y: 0 }}
+      whileHover = {{ y: -5 }}
+      exit = {{ y: 0 }}
+      >
+        <Link href={href}>{children}</Link>
+      </motion.div>
+    )
+  }
+  
+export function Header() {
+    return (
+      <header className="w-full sticky top-0 z-10 py-2 border-b-2 mb-5 border-[#eaeaea] backdrop-blur-sm">
+        <nav className={"max-w-7xl mx-auto flex justify-between items-center"}>
+          <div>
+            <NavButton href="/marketplace">See the Market</NavButton>
+            <NavButton href="/mint">Mint an NFT</NavButton>
+            <NavButton href="/view-nft">View an NFT</NavButton>
+          </div>
+          <ConnectButton />
+        </nav>
+      </header>
+    )
+  }
