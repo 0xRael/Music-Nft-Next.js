69b6369c5349566e39e48d466bd5f5d0a6cfab9e
rael5 <rael1up@gmail.com>
2024-11-19 07:06:02 -0400

    Styling Navbar

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import { Providers } from "@/utils/providers";
 import '@rainbow-me/rainbowkit/styles.css';
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,14 +13,26 @@ export const metadata = {
   description: "Music NFT Marketplace",
 };
 
+function NavButton({ href, children}) {
+  return (
+    <motion.div className="mx-4 hover:underline"
+    initial = {{ y: 0 }}
+    whileHover = {{ y: 30 }}
+    exit = {{ y: 0 }}
+    >
+      <Link href={href}>{children}</Link>
+    </motion.div>
+  )
+}
+
 function Header() {
   return (
-    <header className={"w-full sticky top-0 z-10 py-2 border-b-2 mb-5 border-[#eaeaea]"}>
+    <header className="w-full sticky top-0 z-10 py-2 border-b-2 mb-5 border-[#eaeaea] backdrop-blur-sm">
       <nav className={"max-w-7xl mx-auto flex justify-between items-center"}>
         <div>
-          <Link className={"mx-4"} href="/marketplace">See the Market</Link>
-          <Link className={"mx-4"} href="/mint">Mint an NFT</Link>
-          <Link className={"mx-4"} href="/view-nft">View an NFT</Link>
+          <NavButton href="/marketplace">See the Market</NavButton>
+          <NavButton href="/mint">Mint an NFT</NavButton>
+          <NavButton href="/view-nft">View an NFT</NavButton>
         </div>
         <ConnectButton />
       </nav>
@@ -43,7 +56,7 @@ export default function pageWrapper ({
           </Providers>
         </div>
 
-        <footer className={"w-full fixed bottom-0  border-t-2 border-[#eaeaea] justify-center items-center text-center"}>
+        <footer className="w-full fixed bottom-0  border-t-2 border-[#eaeaea] justify-center items-center text-center backdrop-blur-sm">
 
           Made with &#10084; by 0xRael
         </footer>
